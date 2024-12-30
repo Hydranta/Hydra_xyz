62650aaa18cb36e614dbe2f57435e54d89d0e927
Hydranta <traviscottshop@gmail.com>
2024-12-30 12:14:18 -0500

    Update process.py ~ bits (#8)

--- HydrantaSynapseNeuralNetwork/BitCheck/process.py
+++ HydrantaSynapseNeuralNetwork/BitCheck/process.py
@@ -1,11 +1,13 @@
 import typing as ty
-
 from lava.magma.core.process.process import LogConfig, AbstractProcess
 from lava.magma.core.process.ports.ports import RefPort
 from lava.magma.core.process.variable import Var
 
 
 class BitCheck(AbstractProcess):
+    BITS_MIN = 1
+    BITS_MAX = 31
+
     def __init__(
         self,
         *,
@@ -16,74 +18,69 @@ class BitCheck(AbstractProcess):
         log_config: ty.Optional[LogConfig] = None,
         **kwargs,
     ) -> None:
-        """BitCheck process.
-        This process is used for quick checking of
-        bit-accurate process run as to whether bits will
-        overflow when running on bit limited hardware.
+        """
+        BitCheck process.
+        This process checks for potential bit overflow
+        when running on bit-limited hardware.
 
         Parameters
         ----------
-        shape: Tuple
-            Shape of the sigma process.
-            Default is (1,).
-        layerid: int or float
-            Layer number of network.
-            Default is None.
-        debug: 0 or 1
-            Enable (1) or disable (0) debug print.
-            Default is 0.
-        bits: int
-            Bits to use when checking overflow, 1-32.
-            Default is 24.
+        layerid : Optional[int]
+            Layer ID of the network. Default is None.
+        debug : Optional[int]
+            Enable (1) or disable (0) debug print. Default is 0.
+        bits : Optional[int]
+            Number of bits (1-31) used for overflow checking. Default is 24.
+        name : Optional[str]
+            Name of the process. Default is None.
+        log_config : Optional[LogConfig]
+            Logging configuration. Default is None.
         """
-        super().__init__(
-            name=name,
-            log_config=log_config,
-            **kwargs
-        )
+        super().__init__(name=name, log_config=log_config, **kwargs)
 
-        initial_shape = (1,)
-        self.state = RefPort(initial_shape)
+        self.state = RefPort((1,))
+        self.layerid: Var = Var(shape=(1,), init=layerid)
+        self.debug: Var = Var(shape=(1,), init=debug)
 
-        self.layerid: ty.Type(Var) = Var(shape=initial_shape, init=layerid)
-        self.debug: ty.Type(Var) = Var(shape=initial_shape, init=debug)
-        if bits <= 31 and bits >= 1:
-            self.bits: ty.Type(Var) = Var(shape=initial_shape, init=bits)
-        else:
-            raise ValueError("bits value is \
-                             {} but should be 1-31".format(bits))
-        self._overflowed: ty.Type(Var) = Var(shape=initial_shape, init=0)
+        if not (self.BITS_MIN <= bits <= self.BITS_MAX):
+            raise ValueError(
+                f"Invalid 'bits' value: {bits}. Must be between {self.BITS_MIN} and {self.BITS_MAX}."
+            )
+        self.bits: Var = Var(shape=(1,), init=bits)
+        self._overflowed: Var = Var(shape=(1,), init=0)
 
     def connect_var(self, var: Var) -> None:
+        """
+        Connect a variable to the BitCheck process and reinitialize variables.
+
+        Parameters
+        ----------
+        var : Var
+            The variable to connect.
+        """
         self.state = RefPort(var.shape)
         self.state.connect_var(var)
 
-        self.layerid = Var(name="layerid",
-                           shape=var.shape,
-                           init=self.layerid.init
-                           )
-        self.debug = Var(name="debug",
-                         shape=var.shape,
-                         init=self.debug.init
-                         )
-        self.bits = Var(name="bits",
-                        shape=var.shape,
-                        init=self.bits.init
-                        )
-        self._overflowed = Var(name="overflowed",
-                               shape=var.shape,
-                               init=self._overflowed.init
-                               )
+        self.layerid = Var(name="layerid", shape=var.shape, init=self.layerid.init)
+        self.debug = Var(name="debug", shape=var.shape, init=self.debug.init)
+        self.bits = Var(name="bits", shape=var.shape, init=self.bits.init)
+        self._overflowed = Var(name="overflowed", shape=var.shape, init=self._overflowed.init)
 
         self._post_init()
 
     @property
     def shape(self) -> ty.Tuple[int, ...]:
-        """Return shape of the Process."""
+        """Return the shape of the process."""
         return self.state.shape
 
     @property
-    def overflowed(self) -> ty.Type[int]:
-        """Return overflow Var of Process.
-        1 is overflowed, 0 is not overflowed."""
+    def overflowed(self) -> int:
+        """
+        Return whether the process has overflowed.
+
+        Returns
+        -------
+        int
+            1 if overflowed, 0 otherwise.
+        """
         return self._overflowed.get()
